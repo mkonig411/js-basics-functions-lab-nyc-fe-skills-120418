90978fbf23cfb45be3a7083f3aa6177a1c3a2523
Margot Konig <mkonig411@gmail.com>
2019-01-18 01:24:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 const startingPoint = 42
 
 function distanceFromHqInBlocks (block) {
-  return block - startingPoint
-  if (block < startingPoint)
-}
-function distanceTravelledInFeet(start,end) {
-  console.log (“start: ", start)
-console.log (“end: “, end)
-console.log (start + end) 
-if ((a + b)> 100)
-}
+  if (block < startingPoint) {
+    return (startingPoint - block)
+  }
+  else {
+    return block - startingPoint
+  }}
