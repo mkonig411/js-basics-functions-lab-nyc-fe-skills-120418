7d45fbf1f5265420aaebbd625c03d7a2e27c663b
Margot Konig <mkonig411@gmail.com>
2019-01-18 01:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
+const startingPoint = 42
+
+function distanceFromHqInBlocks ()
 function distanceTravelledInFeet(start,end) {
   console.log (“start: ", start)
 console.log (“end: “, end)
