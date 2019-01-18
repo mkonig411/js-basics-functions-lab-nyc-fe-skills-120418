47fbd894f179105754d39c91091841a50deab36f
Margot Konig <mkonig411@gmail.com>
2019-01-18 01:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ function calculatesFarePrice(start, end) {
     return 0 
   } else if (400 < feetTraveled < 2000) {
     return (feetTraveled - 400) * 0.02
-  }
+  } else if (2000 < feetTraveled < 2500) {
+    return 25
+  } else if 
 }
   
